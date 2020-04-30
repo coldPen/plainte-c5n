2dd7b158c7cf833af1ec191b642708d741184231
Lucas Leroux <lucas.leroux@protonmail.com>
2020-04-30 18:54:09 +0200

    Premier commit <3

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <h1 className="py-4 px-4 bg-blue-600 rounded text-white">Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
