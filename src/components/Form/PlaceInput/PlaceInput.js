be546a3ade1a38c2b191674f78c42ee15f7493ea
Lucas Leroux <lucas.leroux@protonmail.com>
2020-05-03 21:52:29 +0200

    places.js fonctionnel

--- src/components/Form/PlaceInput/PlaceInput.js
+++ src/components/Form/PlaceInput/PlaceInput.js
@@ -9,25 +9,34 @@ const AddressInput = ({
   setValue,
 }) => {
   const inputEl = useRef(null)
-  let placesAutocomplete = null
+  const placesRef = useRef(null)
 
   useEffect(() => {
-    console.log("[PlaceInput] componentDidMount")
-    placesAutocomplete = places({
+    const placesAutocomplete = places({
       appId: process.env.GATSBY_ALGOLIA_APP_ID,
       apiKey: process.env.GATSBY_ALGOLIA_API_KEY,
       container: inputEl.current,
+      type: "address",
+      countries: ["FR"],
     })
-    placesAutocomplete.on("change", () => {
-      console.log(placesAutocomplete.getVal())
-      // setValue(placesAutocomplete.getVal())
-    })
+
+    placesRef.current = placesAutocomplete
+
+    const setValueFromPlaces = () => {
+      setValue(placesRef.current.getVal())
+    }
+
+    placesRef.current.on("change", setValueFromPlaces)
+    placesRef.current.on("clear", setValueFromPlaces)
+
+    return () => {
+      placesRef.current.destroy()
+    }
   }, [])
 
   useEffect(() => {
-    console.log("[PlaceInput] componentDidUpdate")
-    placesAutocomplete.setVal(value)
-  })
+    if (placesRef.current) placesRef.current.setVal(value)
+  }, [value])
 
   return (
     <div>
@@ -37,7 +46,6 @@ const AddressInput = ({
         name={name}
         id={name}
         placeholder={placeholder}
-        // onChange={event => setValue(event.target.value)}
         ref={inputEl}
       />
     </div>
