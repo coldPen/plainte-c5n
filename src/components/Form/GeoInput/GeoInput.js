d88acac6381a870d3ad76c054f80beff5a9cf62d
Lucas Leroux <lucas.leroux@protonmail.com>
2020-05-12 19:31:07 +0200

    [WIP] refactorisation de GeoInput en class pour un debounce adapté à react-autocomplete

--- src/components/Form/GeoInput/GeoInput.js
+++ src/components/Form/GeoInput/GeoInput.js
@@ -1,67 +1,149 @@
-import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
 import axios from "axios";
 import Autosuggest from "react-autosuggest";
+import debounce from "lodash/debounce";
 
-import useDebounce from "../../../hooks/useDebounce";
+// import useDebounce from "../../../hooks/useDebounce";
 
-const geoSearch = (value) => {
-  return axios
+const geoSearch = (value) =>
+  axios
     .get(`https://api-adresse.data.gouv.fr/search/?q=${value}&citycode=13055`)
     .then((resp) => resp.data);
-};
 
-const GeoInput = ({ id, placeholder, value, setValue }) => {
-  const [searchExp, setSearchExp] = useState("");
-  const [results, setResults] = useState([]);
-  const [isSearching, setIsSearching] = useState(false);
+function getSuggestionValue(suggestion) {
+  return suggestion;
+}
 
-  const debouncedSearchExp = useDebounce(searchExp, 500);
+function renderSuggestion(suggestion) {
+  return <span>{suggestion}</span>;
+}
 
-  useEffect(() => {
-    if (debouncedSearchExp) {
-      setIsSearching(true);
-      const encodedSearchExp = encodeURIComponent(debouncedSearchExp).replace(
-        /%20/g,
-        "+"
-      );
-      geoSearch(encodedSearchExp).then((response) => {
-        setIsSearching(false);
-        const results = response.features.map(
-          (feature) => feature.properties.label
-        );
-        setResults(results);
+class GeoInput extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: "",
+      suggestions: [],
+      isLoading: false,
+    };
+
+    this.debouncedLoadSuggestions = debounce(this.loadSuggestions, 500);
+  }
+
+  loadSuggestions(value) {
+    this.setState({
+      isLoading: true,
+    });
+
+    const encodedValue = encodeURIComponent(value).replace(/%20/g, "+");
+
+    geoSearch(encodedValue)
+      .then((response) =>
+        response.features.map((feature) => feature.properties.label)
+      )
+      .then((suggestions) => {
+        if (value === this.state.value) {
+          this.setState({
+            isLoading: false,
+            suggestions,
+          });
+        } else {
+          // Ignore suggestions if input value changed
+          this.setState({
+            isLoading: false,
+          });
+        }
       });
-    } else {
-      setResults([]);
-    }
-  }, [debouncedSearchExp]);
+  }
 
-  const onSuggestionsFetchRequested = ({ value }) => setResults(value);
-  const onSuggestionsClearRequested = () => setResults([]);
+  onChange = (event, { newValue }) => {
+    this.setState({
+      value: newValue,
+    });
+  };
 
-  return (
-    <Autosuggest
-      suggestions={results}
-      onSuggestionsFetchRequested={onSuggestionsFetchRequested}
-      onSuggestionsClearRequested={onSuggestionsClearRequested}
-    />
-    // <>
-    //   <input
-    //     type="text"
-    //     name={id}
-    //     id={id}
-    //     placeholder={placeholder}
-    //     //   value={value}
-    //     onChange={(event) => setSearchExp(event.target.value)}
-    //   />
-    //   {isSearching && <div>Searching ...</div>}
-    //   <ul>
-    //     {results.map((result) => (
-    //       <li>{result}</li>
-    //     ))}
-    //   </ul>
-    // </>
-  );
-};
+  onSuggestionsFetchRequested = ({ value }) => {
+    this.debouncedLoadSuggestions(value);
+  };
+
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: [],
+    });
+  };
+
+  render() {
+    const { value, suggestions, isLoading } = this.state;
+    const inputProps = {
+      placeholder: "Type something",
+      value,
+      onChange: this.onChange,
+    };
+    const status = isLoading ? "Loading..." : "Type to load suggestions";
+
+    return (
+      <div>
+        <div className="status">
+          <strong>Status:</strong> {status}
+        </div>
+        <Autosuggest
+          suggestions={suggestions}
+          onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+          onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+          getSuggestionValue={getSuggestionValue}
+          renderSuggestion={renderSuggestion}
+          inputProps={inputProps}
+        />
+      </div>
+    );
+  }
+}
+
+// const GeoInput = ({ id, placeholder, value, setValue }) => {
+//   const [searchExp, setSearchExp] = useState("");
+//   const [suggestions, setSuggestions] = useState([]);
+//   const [isSearching, setIsSearching] = useState(false);
+
+//   const debouncedSearchExp = useDebounce(searchExp, 500);
+
+//   useEffect(() => {
+//     if (debouncedSearchExp) {
+//       setIsSearching(true);
+//       const encodedSearchExp = encodeURIComponent(debouncedSearchExp).replace(
+//         /%20/g,
+//         "+"
+//       );
+//       geoSearch(encodedSearchExp).then((response) => {
+//         setIsSearching(false);
+//         const results = response.features.map(
+//           (feature) => feature.properties.label
+//         );
+//         setSuggestions(results);
+//       });
+//     } else {
+//       setSuggestions([]);
+//     }
+//   }, [debouncedSearchExp]);
+
+//   return (
+//     <>
+//       <input
+//         type="text"
+//         name={id}
+//         id={id}
+//         placeholder={placeholder}
+//         //   value={value}
+//         onChange={(event) => setSearchExp(event.target.value)}
+//       />
+//       {isSearching && <div>Searching ...</div>}
+//       <ul>
+//         {suggestions.map((suggestion) => (
+//           <li>{suggestion}</li>
+//         ))}
+//       </ul>
+//     </>
+//   );
+// };
 
 export default GeoInput;
